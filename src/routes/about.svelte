be61525a061fa83cdfc4470ef8ee7200998b4bc1
xpat <cheftech@pinchepoutine.digital>
2021-01-07 20:56:42 -0500

    first commit

---
+++ src/routes/about.svelte
@@ -0,0 +1,7 @@
+<svelte:head>
+	<title>About</title>
+</svelte:head>
+
+<h1>About this site</h1>
+
+<p>This is an exported static site build with .svelte files and sapper. It mostly serves as a keep of links to important domains.</p>
